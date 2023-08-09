7c126c766d61b8b94c8323cb5f28cdc120eec190
deepbajud <dbajud@gmail.com>
2023-08-09 22:03:08 +0530

    commit 5

---
+++ app/src/main/java/com/example/dchatbajud/Chat_Activity.java
@@ -0,0 +1,14 @@
+package com.example.dchatbajud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Chat_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
