c1ef9e98f395cb20f7542d5517feea2d64ee0607
deepbajud <dbajud@gmail.com>
2023-08-09 16:21:54 +0530

    commit 3

---
+++ app/src/main/java/com/example/dchatbajud/Set_Profile.java
@@ -0,0 +1,14 @@
+package com.example.dchatbajud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Set_Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_profile);
+    }
+}
